2808931107b6b2c3df6591c238ff898cc95be4fd
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-12-02 21:44:52 +0100

    Copy Static Token Map

---
+++ packages/agent-sdk/src/evm/tokens.ts
@@ -0,0 +1,22 @@
+import { isAddress } from "viem";
+// TODO: Move outside of src folder.
+import tokenMap from "./tokenMap.json";
+import { getTokenInfo, type TokenInfo } from "@bitteprotocol/agent-sdk";
+
+type SymbolMapping = Record<string, TokenInfo>;
+type ChainId = number;
+export type BlockchainMapping = Record<ChainId, SymbolMapping>;
+
+export async function getTokenDetails(
+  chainId: number,
+  symbolOrAddress: string,
+): Promise<TokenInfo> {
+  if (isAddress(symbolOrAddress, { strict: false })) {
+    return getTokenInfo(chainId, symbolOrAddress);
+  }
+  console.log("Seeking TokenMap for Symbol", symbolOrAddress);
+  return (tokenMap as BlockchainMapping)[chainId][
+    // TokenMap has lower cased (sanitized) symbols
+    symbolOrAddress.toLowerCase()
+  ];
+}
