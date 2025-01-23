68e9d15a482cfe45964d3899411fa8066b34490f
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-01-23 14:05:45 +0100

    add null transaction

--- packages/agent-sdk/src/index.ts
+++ packages/agent-sdk/src/index.ts
@@ -2,3 +2,4 @@ export * from "./validate";
 export * from "./evm";
 export * from "./request";
 export * from "./error";
+export * from "./misc";
---
+++ packages/agent-sdk/src/misc.ts
@@ -0,0 +1,7 @@
+import { MetaTransaction } from "near-safe";
+
+export const NULL_TRANSACTION: MetaTransaction = {
+  to: "0x0000000000000000000000000000000000000000",
+  value: "0x00",
+  data: "0x",
+};
