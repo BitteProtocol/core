6ed98b1a0ddd1198ae4c6127292be0db14b2dbbf
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-09-10 11:59:02 +0200

    Add Near Constants

---
+++ packages/agent-sdk/src/near/constants.ts
@@ -0,0 +1,19 @@
+export declare const DEFAULT_GAS = "200000000000000";
+export declare const MAX_GAS = "300000000000000";
+export declare const ONE_YOCTO = "1";
+export declare const TOKEN_CONTRACT_SPEC = "nft-1.0.0";
+export declare const DEPLOY_CONTRACT_V1_DEPOSIT: string;
+export declare const DEPLOY_CONTRACT_V2_DEPOSIT: string;
+export declare const LISTING_DEPOSIT: string;
+export declare const FT_STORAGE_DEPOSIT: string;
+export const GAS_CONSTANTS = {
+  DEFAULT_GAS,
+  OPTIMAL_GAS: "225000000000000",
+  MAX_GAS,
+  FT_TRANSFER: "15000000000000",
+};
+export declare const STORAGE_PRICE_PER_BYTE_EXPONENT = 19;
+export declare const DEPOSIT_FOR_BURN = "1";
+export declare const GAS_FOR_BURN = "200000000000000";
+export declare const DEPOSIT_FOR_TRANSFER = "1";
+export declare const GAS_FOR_TRANSFER = "200000000000000";
