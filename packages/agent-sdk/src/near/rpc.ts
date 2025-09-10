0e7b3b93a549374a5dee6d2c49ec632e3f3b0f5b
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-09-10 11:42:23 +0200

    Add rpc methods

---
+++ packages/agent-sdk/src/near/rpc.ts
@@ -0,0 +1,45 @@
+import { Account, providers } from "near-api-js";
+
+export interface RpcParams {
+  rpcUrl?: string;
+}
+
+export interface AccountParams extends RpcParams {
+  accountId: string;
+}
+export interface FTParams extends AccountParams {
+  contractId: string;
+}
+
+export async function getBalance({
+  accountId,
+  rpcUrl,
+}: AccountParams): Promise<bigint> {
+  const provider = new providers.JsonRpcProvider({
+    url: rpcUrl || "https://rpc.mainnet.near.org",
+  });
+  const account = new Account(accountId, provider);
+  const { balance } = await account.getState();
+  return balance.available;
+}
+
+export async function ftBalance({
+  contractId,
+  accountId,
+  rpcUrl,
+}: FTParams): Promise<string> {
+  const provider = new providers.JsonRpcProvider({
+    url: rpcUrl || "https://rpc.mainnet.near.org",
+  });
+  const result = await provider.callFunction<string>(
+    contractId,
+    "ft_balance_of",
+    { account_id: accountId },
+  );
+  if (!result) {
+    throw new Error(
+      `Could not read ftBalance of ${accountId} for ${contractId}`,
+    );
+  }
+  return result;
+}
