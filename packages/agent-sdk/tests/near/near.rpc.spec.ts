550c9e344722963a1f6cc78c11042b975ab410b7
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-09-10 11:43:14 +0200

    format code

--- packages/agent-sdk/tests/near/near.rpc.spec.ts
+++ packages/agent-sdk/tests/near/near.rpc.spec.ts
@@ -2,22 +2,31 @@ import { getBalance, ftBalance } from "../../src/near";
 
 describe("near utilities", () => {
   it("getBalance success", async () => {
-    expect(await getBalance({accountId: "max-normal.near"})).toBeDefined();
+    expect(await getBalance({ accountId: "max-normal.near" })).toBeDefined();
   });
 
   it("getBalance fail", async () => {
-    await expect(() => getBalance({accountId: "zebra"})).toThrow();
+    await expect(() => getBalance({ accountId: "zebra" })).toThrow();
   });
 
   it("ftBalance success", async () => {
-    expect(await ftBalance({accountId: "max-normal.near", contractId: "wrap.near"})).toBeDefined();
+    expect(
+      await ftBalance({
+        accountId: "max-normal.near",
+        contractId: "wrap.near",
+      }),
+    ).toBeDefined();
   });
 
   it("ftBalance zero", async () => {
-    expect(await ftBalance({accountId: "zebra", contractId: "wrap.near"})).toBe("0");
+    expect(
+      await ftBalance({ accountId: "zebra", contractId: "wrap.near" }),
+    ).toBe("0");
   });
 
   it("ftBalance fail", async () => {
-    await expect(() => ftBalance({accountId: "wrap.near", contractId: "zebra"})).toThrow();
+    await expect(() =>
+      ftBalance({ accountId: "wrap.near", contractId: "zebra" }),
+    ).toThrow();
   });
 });
